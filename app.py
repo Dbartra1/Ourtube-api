7a2b9f4f3f84262c668bdcfb7634ff30edf0e8c6
Dbartra1 <dfbartram1@gmail.com>
2023-02-23 11:34:16 -0700

    Added test json, tested in postman, updated ticket.

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+##@app.route('/hello/', methods=['GET', 'POST'])
+##def welcome():
+    ##return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == "GET":
+        return jsonify({"response": "Get Request Called!"})
+    elif request.method == "POST": 
+        req_Json = request.json
+        name = req_Json ['name']
+        return jsonify({"response": "Hi" + name})
+
+if __name__ == '__main__': 
+    app.run(debug=True, port=9090)
+
+##if __name__ == '__main__':
+    ##app.run(host='0.0.0.0', port=105)
