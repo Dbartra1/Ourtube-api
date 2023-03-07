48bfbea96a26747b83457501ed1eb8e74349ffc2
Dbartra1 <dfbartram1@gmail.com>
2023-03-06 23:03:41 -0700

    Added download function to pytube testing

--- PytubeTesting.py
+++ PytubeTesting.py
@@ -1,6 +1,11 @@
-from pytube import YouTube, Search 
+from pytube import YouTube, Search, Stream
 import urllib.request
 from PIL import Image
+from pathlib import Path
+
+## Finding users download path
+
+downloads_path = str(Path.home() / "Downloads")
 
 ## Pulling a video and it's information via a implicit URL
 
@@ -8,34 +13,42 @@ yt = YouTube(input("enter url:"))
 title = yt.title
 print (title)
 
-## Using the search module
+## Stream downloading test 
 
-s = Search(input("What do you want?: "))
-print ("There were", len(s.results), "results found") 
-print (s.results)
-x = input("Did you find what you were looking for?: ")
-if x == "no":
-    s.get_next_results()
-    print ("There were", len(s.results), " additional results found") 
-else: 
-    print ("thanks for using the thing, goodbye!")
+yt.streams
 
-##creating an object with the video ID
+stream = yt.streams.get_highest_resolution()
 
-yt_id = YouTube.from_id("XubH4QNdUjQ") 
+stream.download()
 
-print (yt_id.title) 
+# ## Using the search module
 
-##making sure that we can grab an image URL
-print (yt_id.thumbnail_url)
+# s = Search(input("What do you want?: "))
+# print ("There were", len(s.results), "results found") 
+# print (s.results)
+# x = input("Did you find what you were looking for?: ")
+# if x == "no":
+#     s.get_next_results()
+#     print ("There were", len(s.results), " additional results found") 
+# else: 
+#     print ("thanks for using the thing, goodbye!")
+
+# ##creating an object with the video ID
+
+# yt_id = YouTube.from_id("XubH4QNdUjQ") 
+
+# print (yt_id.title) 
+
+# ##making sure that we can grab an image URL
+# print (yt_id.thumbnail_url)
 
 
-##display the image 
-urllib.request.urlretrieve(
-  'https://i.ytimg.com/vi/XubH4QNdUjQ/sddefault.jpg',
-    "sddefault.jpg")
+# ##display the image 
+# urllib.request.urlretrieve(
+#   'https://i.ytimg.com/vi/XubH4QNdUjQ/sddefault.jpg',
+#     "sddefault.jpg")
   
-img = Image.open("sddefault.jpg")
-img.show()
+# img = Image.open("sddefault.jpg")
+# img.show()
 
 
